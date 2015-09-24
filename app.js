aaba65b326411e7034bf33663dc99bc650e91b2d
OlehHappy <olegtranslator@gmail.com>
2015-09-24 00:55:10 -0500

    added gulp to project

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.send('wellcome to my API!');
+    res.send('wellcome to my API! Test');
 });
 
 app.listen(port, function() {
